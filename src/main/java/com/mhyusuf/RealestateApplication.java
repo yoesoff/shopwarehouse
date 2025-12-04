a583eb255811e61128db023989b385e8234bed38
M Yusuf Ibrahim <yusuf.ibrahim@silverlakeaxis.com>
2025-12-04 12:00:17 +0700

    First setup for RealestateApplication

--- src/main/java/com/mhyusuf/RealestateApplication.java
+++ src/main/java/com/mhyusuf/RealestateApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShopWarehouseApplication {
+public class RealestateApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ShopWarehouseApplication.class, args);
+        SpringApplication.run(RealestateApplication.class, args);
     }
 
 }
